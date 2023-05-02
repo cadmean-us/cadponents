dd8f31096b1f957089c104e6aae04d647eded1a9
nastakuz <nastakuz2015@gmail.com>
2023-05-02 15:35:41 +0700

    menu update

--- src/lib/chips/Chip.svelte
+++ src/lib/chips/Chip.svelte
@@ -1,19 +1,21 @@
 <script>
-    import { createEventDispatcher } from 'svelte';
-    import Close from "$lib/icons/Close.svelte";
-    import {fade } from 'svelte/transition';
+	import { createEventDispatcher } from 'svelte';
+	import Close from '$lib/icons/Close.svelte';
+	import { fade } from 'svelte/transition';
 
-    const dispatch = createEventDispatcher();
-    export let id
-    export let value
-    function remove() {
-        dispatch('remove', id);
-    }
-
+	const dispatch = createEventDispatcher();
+	export let id;
+	export let value;
+	function remove() {
+		dispatch('remove', id);
+	}
 </script>
 
-<div class="rounded-lg flex gap-[10px] items-center justify-center text-[#282945] bg-[#ECDEFF] w-fit px-[12px] py-[6px]"
-    in:fade={{ duration: 200 }} out:fade={{ duration: 200 }}>
-    <slot>{value}</slot>
-    <Close on:click={remove}/>
+<div
+	class="rounded-lg flex gap-[10px] items-center justify-center text-[#282945] bg-[#ECDEFF] w-fit px-[12px] py-[6px]"
+	in:fade={{ duration: 200 }}
+	out:fade={{ duration: 200 }}
+>
+	<slot>{value}</slot>
+	<Close class="cursor-pointer" on:click={remove} />
 </div>
