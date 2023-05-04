acd4ebaf72732289a631d1a113abad2ab7538a62
nastakuz <nastakuz2015@gmail.com>
2023-05-04 17:43:25 +0700

    side menu update

--- src/lib/menu/SideMenuTab.svelte
+++ src/lib/menu/SideMenuTab.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	export let href;
 	export let name;
 	export let icon;
 
 	export let activeColor;
 	export let inactiveColor;
+	export let padding;
 
 	let fill = inactiveColor;
 
 	function handleMouseOver(_) {
 		fill = activeColor;
+		this.style.color = fill;
 	}
 
 	function handleMouseOut(_) {
 		fill = inactiveColor;
+		this.style.color = fill;
 	}
+
+	onMount(() => {
+		fill = location.pathname.includes(`/${name}`) ? activeColor : inactiveColor;
+	});
 </script>
 
-<li>
+<li style="margin-bottom: {padding}px;">
 	<a
-		class="flex items-center p-2 text-[#66667C] hover:text-white rounded-lg hover:no-underline"
+		class="flex items-center p-2 rounded-lg hover:no-underline"
+		style="color: {fill}"
 		{href}
 		on:mouseout={handleMouseOut}
 		on:mouseover={handleMouseOver}
