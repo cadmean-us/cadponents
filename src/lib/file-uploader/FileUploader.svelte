d56ff9a8b9920300b9638f3c3660128a4986ed41
George <kotlyar@cadmean.dev>
2023-05-19 21:27:36 +0700

    file uploader

---
+++ src/lib/file-uploader/FileUploader.svelte
@@ -0,0 +1,28 @@
+<script>
+	export let files;
+	export let multiple;
+	export let accept = [];
+
+	/**
+	 * Override the default behavior of validating uploaded files.
+	 * By default, files are not validated
+	 * @type {(files: ReadonlyArray<File>) => ReadonlyArray<File>}
+	 */
+	export let validateFiles = (files) => files;
+</script>
+
+<input
+	type="file"
+	tabindex="-1"
+	{accept}
+	{name}
+	{multiple}
+	class:bx--file-input={true}
+	on:change={({ target }) => {
+		files = validateFiles([...target.files]);
+	}}
+	on:click
+	on:click={({ target }) => {
+		target.value = null;
+	}}
+/>
---
+++ src/routes/fileuploader/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import FileUploader from '$lib/file-uploader/FileUploader.svelte';
+</script>
+
+<FileUploader />
