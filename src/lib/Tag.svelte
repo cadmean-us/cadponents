755446941ecf73c4473e4fb00bb7b7a8cf201f51
Vyacheslav Lutsenko <84517362+SlaweekLut@users.noreply.github.com>
2024-06-20 17:49:16 +0500

    feat: rework tags
    delete extra props

--- src/lib/Tag.svelte
+++ src/lib/Tag.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	export let type: 'accent' | 'muted' | 'neutral' | 'border' = 'accent';
-	export let disabled = false;
+	export let variant: 'accent' | 'muted' | 'neutral' | 'border' = 'accent';
 </script>
 
-<button {...$$restProps} class="tag tag--{type}" disabled={disabled}>
+<button {...$$restProps} class="tag tag--{variant} {$$restProps.class}">
 	<slot />
 </button>
 
--- src/routes/tag/+page.svelte
+++ src/routes/tag/+page.svelte
@@ -7,51 +7,51 @@
 
 <div class="ui-group">
 	<div class="ui-group__row">
-		<Tag type="muted">Muted</Tag>
-		<Tag type="accent">Accent</Tag>
-		<Tag type="neutral">Neutral</Tag>
-		<Tag type="border">Border</Tag>
+		<Tag variant="muted">Muted</Tag>
+		<Tag variant="accent">Accent</Tag>
+		<Tag variant="neutral">Neutral</Tag>
+		<Tag variant="border">Border</Tag>
 	</div>
 	<div class="ui-group__row">
-		<Tag type="muted" disabled>Muted</Tag>
-		<Tag type="accent" disabled>Accent</Tag>
-		<Tag type="neutral" disabled>Neutral</Tag>
-		<Tag type="border" disabled>Border</Tag>
+		<Tag variant="muted" disabled>Muted</Tag>
+		<Tag variant="accent" disabled>Accent</Tag>
+		<Tag variant="neutral" disabled>Neutral</Tag>
+		<Tag variant="border" disabled>Border</Tag>
 	</div>
 	<div class="ui-group__row">
-		<Tag type="muted">Muted <Placeholder/></Tag>
-		<Tag type="accent">Accent <Placeholder/></Tag>
-		<Tag type="neutral">Neutral <Placeholder/></Tag>
-		<Tag type="border">Border <Placeholder/></Tag>
+		<Tag variant="muted">Muted <Placeholder/></Tag>
+		<Tag variant="accent">Accent <Placeholder/></Tag>
+		<Tag variant="neutral">Neutral <Placeholder/></Tag>
+		<Tag variant="border">Border <Placeholder/></Tag>
 	</div>
 	<div class="ui-group__row">
-		<Tag type="muted" disabled>Muted <Placeholder/></Tag>
-		<Tag type="accent" disabled>Accent <Placeholder/></Tag>
-		<Tag type="neutral" disabled>Neutral <Placeholder/></Tag>
-		<Tag type="border" disabled>Border <Placeholder/></Tag>
+		<Tag variant="muted" disabled>Muted <Placeholder/></Tag>
+		<Tag variant="accent" disabled>Accent <Placeholder/></Tag>
+		<Tag variant="neutral" disabled>Neutral <Placeholder/></Tag>
+		<Tag variant="border" disabled>Border <Placeholder/></Tag>
 	</div>
 	<div class="ui-group__row">
-		<Tag type="muted"><Placeholder/> Muted</Tag>
-		<Tag type="accent"><Placeholder/> Accent</Tag>
-		<Tag type="neutral"><Placeholder/> Neutral</Tag>
-		<Tag type="border"><Placeholder/> Border</Tag>
+		<Tag variant="muted"><Placeholder/> Muted</Tag>
+		<Tag variant="accent"><Placeholder/> Accent</Tag>
+		<Tag variant="neutral"><Placeholder/> Neutral</Tag>
+		<Tag variant="border"><Placeholder/> Border</Tag>
 	</div>
 	<div class="ui-group__row">
-		<Tag type="muted" disabled><Placeholder/> Muted</Tag>
-		<Tag type="accent" disabled><Placeholder/> Accent</Tag>
-		<Tag type="neutral" disabled><Placeholder/> Neutral</Tag>
-		<Tag type="border" disabled><Placeholder/> Border</Tag>
+		<Tag variant="muted" disabled><Placeholder/> Muted</Tag>
+		<Tag variant="accent" disabled><Placeholder/> Accent</Tag>
+		<Tag variant="neutral" disabled><Placeholder/> Neutral</Tag>
+		<Tag variant="border" disabled><Placeholder/> Border</Tag>
 	</div>
 	<div class="ui-group__row">
-		<Tag type="muted"><Placeholder/> Muted <Placeholder/></Tag>
-		<Tag type="accent"><Placeholder/> Accent <Placeholder/></Tag>
-		<Tag type="neutral"><Placeholder/> Neutral <Placeholder/></Tag>
-		<Tag type="border"><Placeholder/> Border <Placeholder/></Tag>
+		<Tag variant="muted"><Placeholder/> Muted <Placeholder/></Tag>
+		<Tag variant="accent"><Placeholder/> Accent <Placeholder/></Tag>
+		<Tag variant="neutral"><Placeholder/> Neutral <Placeholder/></Tag>
+		<Tag variant="border"><Placeholder/> Border <Placeholder/></Tag>
 	</div>
 	<div class="ui-group__row">
-		<Tag type="muted" disabled><Placeholder/> Muted <Placeholder/></Tag>
-		<Tag type="accent" disabled><Placeholder/> Accent <Placeholder/></Tag>
-		<Tag type="neutral" disabled><Placeholder/> Neutral <Placeholder/></Tag>
-		<Tag type="border" disabled><Placeholder/> Border <Placeholder/></Tag>
+		<Tag variant="muted" disabled><Placeholder/> Muted <Placeholder/></Tag>
+		<Tag variant="accent" disabled><Placeholder/> Accent <Placeholder/></Tag>
+		<Tag variant="neutral" disabled><Placeholder/> Neutral <Placeholder/></Tag>
+		<Tag variant="border" disabled><Placeholder/> Border <Placeholder/></Tag>
 	</div>
 </div>
